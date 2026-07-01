bc20eb912547b26df708cff07c6afee93062b92c
Valeev Azamat <vazamat@gmail.com>
2026-07-01 22:39:34 +0300

    minor corrections

--- Cython/setup.py
+++ Cython/setup.py
@@ -4,6 +4,6 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules=cythonize("math.pyx"),
+    ext_modules=cythonize("my_math.pyx"),
     include_dirs=[np.get_include()] # include NumPy headers
 )
